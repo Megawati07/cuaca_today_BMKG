414f2b48aac9051558ee5d17fbff18bd8af5f3a2
egamegaa <mw.ramly@gmail.com>
2023-06-10 15:08:10 +0700

    cuaca

--- cuaca_today_scrap.R
+++ cuaca_today_scrap.R
@@ -16,34 +16,30 @@ data
 
 # Mendefinisikan nama variabel
 data2 <- data[[2]]
-data3 <- data2[-c(2,3,4,5),]
-colnames(data3)<- c("X1" = "Kota", "X2" = "Perkiraan Cuaca Pagi", "X3" = "Perkiraan Cuaca Siang","X4" = "Perkiraan Cuaca Malam", "X5" = "Perkiraan Cuaca Dini Hari", "X6" = "Suhu", "X7" = "Kelembapan" )
-data3
+nc<-ncol(data2)
+data3 <- data2[-c(2,3,4,5),c(1,2,nc-1,nc)] # hanya mengambil perkiraan cuaca siang
+colnames(data3)<- c("X1" = "Kota", "X2" = "Perkiraan Cuaca Siang", "X3" = "Suhu", "X4" = "Kelembapan" )
 
 # Memisahkan Variabel Suhu menjadi minimum dan maksimum
 d1 <- data.frame(do.call('rbind', strsplit(as.character(data3$`Suhu`),'-',fixed=TRUE)))
 colnames(d1)<- c("X1" = "Suhu_Minimum", "X2" = "Suhu_Maksimum")
-d1
 
 # Memisahkan Variabel Kelembapan menjadi minimum dan maksimum
 d2 <- data.frame(do.call('rbind', strsplit(as.character(data3$`Kelembapan`),'-',fixed=TRUE)))
 colnames(d2)<- c("X1" = "Kelembapan_Minimum", "X2" = "Kelembapan_Maksimum")
-d2
 
 # Mengganti variabel Suhu dan Kelembapan dengan variabel suhu min max
-data4 <- cbind(data3[,-c(6,7)], d1, d2)
-data4
+data4 <- cbind(data3[,-c(3,4)], d1, d2)
 
 # Mengubah variabel-variabel tertentu menjadi numerik
 variabel_ubah <- c("Suhu_Minimum", "Suhu_Maksimum", "Kelembapan_Minimum", "Kelembapan_Maksimum" )  
 data4[,variabel_ubah] <- sapply(data4[,variabel_ubah], as.numeric)
-data4
 
 # Mengurutkan data berdasarkan suhu maksimum
 datafix <- data4[order(data4$Suhu_Maksimum,decreasing = T),]
 
 # Mongo db 
-massage('Input Data ke MongoDB Atlas')
+message('Input Data ke MongoDB Atlas')
 atlas_connect <- mongo(
   collection = Sys.getenv("ATLAS_COLLECTION"),
   db         = Sys.getenv("ATLAS_DB"),
@@ -52,3 +48,4 @@ atlas_connect <- mongo(
 
 atlas_connect$insert(datafix)
 rm(atlas_connect)
+
